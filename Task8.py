3d7a0d9a936b60a9c0c45c4b3163e32afa08c74f
Igor Kolosov <59646564+irbovch@users.noreply.github.com>
2022-10-09 20:43:53 +0300

    Add files via upload
    Тренировочный ЕГЭ с сайта Полякова. 
    
    (Е. Джобс) Вася составляет 4-буквенные слова из букв И, Н, С, Т, А, В, К и упорядочивает их по алфавиту. При этом на первом месте может быть только согласная, на последнем - гласная. Вот начало списка:
    1. ВААА
    2. ВААИ
    3. ВАВА
    ...
    Укажите номер слова НИКА в этом списке.

---
+++ Task8.py
@@ -0,0 +1,11 @@
+t=sorted('И, Н, С, Т, А, В, К'.split(', '))
+l=[]
+for i1 in t:
+   for i2 in t:
+       for i3 in t:
+           for i4 in t:
+               if i1 in 'НСТВК' and i4 in 'ИА':
+                   l.append(i1+i2+i3+i4)
+item = 'НИКА'
+index = l.index(item)
+print ("Ответ:", index)
